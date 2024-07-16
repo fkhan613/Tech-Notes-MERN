6a0f90c9976a931b45b990bfa466ff36ad399042
Farhan Khan <farhan.k2005@gmail.com>
2024-07-16 00:36:33 -0400

    cnage api url

--- frontend/src/app/api/apiSlice.js
+++ frontend/src/app/api/apiSlice.js
@@ -2,7 +2,7 @@ import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 import { setCredentials } from "../../features/auth/authSlice";
 
 const baseQuery = fetchBaseQuery({
-  baseUrl: "https://tech-notes-mern.onrender.com",
+  baseUrl: "https://tech-notes-mern-api.onrender.com",
   credentials: "include",
   prepareHeaders: (headers, { getState }) => {
     const token = getState().auth.token;
