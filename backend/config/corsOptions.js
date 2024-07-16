79c8a624bf71cde72dc9833c931d392afe2b6e3b
Farhan Khan <farhan.k2005@gmail.com>
2024-07-16 00:51:30 -0400

    bug fix

--- backend/config/corsOptions.js
+++ backend/config/corsOptions.js
@@ -1,17 +1,15 @@
-import allowedOrigins from './allowedOrigins';
+const allowedOrigins = require("./allowedOrigins");
 
 const corsOptions = {
-    origin: (origin, callback) => {
-        if (allowedOrigins.indexOf(origin) !== -1 || !origin){
-            callback(null, true)
-        } else{
-            callback(new Error('Not allowed by CORS'))
-        }
-    },
+  origin: (origin, callback) => {
+    if (allowedOrigins.indexOf(origin) !== -1 || !origin) {
+      callback(null, true);
+    } else {
+      callback(new Error("Not allowed by CORS"));
+    }
+  },
+  credentials: true,
+  optionsSuccessStatus: 200,
+};
 
-    credentials:true,
-    optionsSuccessStatus: 200
-
-}
-
-export default corsOptions;
+module.exports = corsOptions;
