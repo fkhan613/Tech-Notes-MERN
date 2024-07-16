49f8eeda7018ecb49ca2651766c1155607f1069e
Farhan Khan <farhan.k2005@gmail.com>
2024-07-16 00:55:44 -0400

    sdfsdf

--- backend/routes/noteRoutes.js
+++ backend/routes/noteRoutes.js
@@ -1,4 +1,4 @@
-import express from "express";
+const express = require("express");
 const router = express.Router();
 import notesController from "../controllers/notesController";
 import verifyJWT from "../middleware/verifyJWT";
